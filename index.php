10cf50c072578d9f93b0e54b97cb5a6843e2a7ce
Bernat Bonda <bondabear@gmail.com>
2018-02-13 23:25:37 +0100

    CSS classes

--- index.php
+++ index.php
@@ -2,34 +2,54 @@
 <HEAD>
     <TITLE>SkyXplore</TITLE>
     <META charset="utf-8">
+    <LINK href="css/reset.css" rel="stylesheet">
     <LINK href="css/common.css" rel="stylesheet">
-    <LINK href="css/index/index_style.css" rel="stylesheet">
+    <LINK href="css/classes.css" rel="stylesheet">
+    
+    <STYLE>
+        @media screen and (max-width: 1000px){
+            main{
+                position: static !important;
+            }
+            #logincontainer, #registrationcontainer{
+                float: none;
+                border: none;
+                border-bottom: 5px ridge rgb(100,100,100);
+                width: 100%;
+                margin: 0;
+            }
+            
+            label{
+                text-align: center !important;
+            }
+        }
+    </STYLE>
 </HEAD>
 <BODY>
-    <MAIN>
+    <MAIN class='absolute20 backgroundblack8 border5px borderridge bordercolor100 overflowauto'>
         <H1>SkyXplore</H1>
-        <SECTION id="logincontainer">
-            <H2>Bejelentkezés</H2>
+        <SECTION class='border5px borderbottomridge borderrightridge bordercolor100 centertext floatleft marginleft025rem marginright025rem padding025rem width230rem' id="logincontainer">
+            <H2 class='centertext fontsize20rem'>Bejelentkezés</H2>
             <FORM method="POST" action="login/php/login.php" id="login" onsubmit="validateLoginData(event)">
-                <LABEL>Felhasználónév: <INPUT type="text" name="loginusername" placeholder="Felhasználónév" required id="loginusername"></LABEL>
-                <LABEL>Jelszó: <INPUT type="password" name="loginpassword" placeholder="Jelszó" required id="loginpassword"></LABEL>
-                <BUTTON>Bejelentkezés</BUTTON>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>Felhasználónév: <INPUT class='fontsize0875rem' type="text" name="loginusername" placeholder="Felhasználónév" required id="loginusername"></LABEL>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>Jelszó: <INPUT class='fontsize0875rem' type="password" name="loginpassword" placeholder="Jelszó" required id="loginpassword"></LABEL>
+                <BUTTON class='marginbottom025rem'>Bejelentkezés</BUTTON>
             </FORM>
             <FORM method="GET" action="login/forgotten.php">
-                <BUTTON>Elfelejtett belépési adatok</BUTTON>
+                <BUTTON class='marginbottom025rem'>Elfelejtett belépési adatok</BUTTON>
             </FORM>
         </SECTION>
-        <SECTION id="registrationcontainer">
-            <H2>Regisztráció</H2>
+        <SECTION class='border5px borderbottomridge borderleftridge bordercolor100 centertext floatright marginleft025rem marginright025rem padding025rem width230rem' id="registrationcontainer">
+            <H2 class='centertext fontsize20rem'>Regisztráció</H2>
             <FORM method="POST" action="login/php/registration.php" onsubmit="validateRegistrationData(event)">
-                <LABEL>Felhasználónév: <INPUT type="text" name="regusername" placeholder="Felhasználónév" required id="regusername"></LABEL>
-                <LABEL>E-mail cím: <INPUT type="text" name="regemail" placeholder="E-mail cím" required id="regemail"></LABEL>
-                <LABEL>Jelszó: <INPUT type="password" name="regpassword1" placeholder="Jelszó" required id="regpassword1"></LABEL>
-                <LABEL>Jelszó újra: <INPUT type="password" name="regpassword2" placeholder="Jelszó újra" required id="regpassword2"></LABEL>
-                <BUTTON>Regisztráció</BUTTON>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>Felhasználónév: <INPUT class='fontsize0875rem' type="text" name="regusername" placeholder="Felhasználónév" required id="regusername"></LABEL>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>E-mail cím: <INPUT class='fontsize0875rem' type="text" name="regemail" placeholder="E-mail cím" required id="regemail"></LABEL>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>Jelszó: <INPUT class='fontsize0875rem' type="password" name="regpassword1" placeholder="Jelszó" required id="regpassword1"></LABEL>
+                <LABEL class='block fontsize15rem marginbottom025rem righttext'>Jelszó újra: <INPUT class='fontsize0875rem' type="password" name="regpassword2" placeholder="Jelszó újra" required id="regpassword2"></LABEL>
+                <BUTTON class='marginbottom025rem'>Regisztráció</BUTTON>
             </FORM>
         </SECTION>
-        <SECTION class="introduction">
+        <SECTION class="fontsize15rem justifytext padding025rem">
             2317-et írunk. Hosszas fejlesztések és kísérletezések során mérnökeink kifejlesztettek egy új galaktikus átjárót, melynek segítségével pillanatok alatt bárhová el lehet jutni az univerzumban. Ez lehetőségek sokaságát jelenti a felfedezók és kalandvadászok számára. Sorra alapulnak az űrkutató társaságok, mik pilóták ezreit alkalmazzák, és folyamatosan keresik a vállalkozó kedvű fiatalokat, kik a csillagok közé vágynak. De ezek az utazások veszélyeket is rejtenek magukban: agresszív idegen létformák és ellenséges társulatok támadhatnak az távoli egeken repülőkre. Elég bátor vagy ahhoz, hogy szembenézz az ismeretlennel? Csatlakozz Te is a rettenthetetlen pilóták csapatához, és légy Te a Világegyetem legjobbja!
         </SECTION>
     </MAIN>
--- login/forgotten.php
+++ login/forgotten.php
@@ -2,19 +2,28 @@
 <HEAD>
     <TITLE>Elfelejtett belépési adatok</TITLE>
     <META charset="utf-8">
+    <LINK href="../css/reset.css" rel="stylesheet">
     <LINK href="../css/common.css" rel="stylesheet">
-    <LINK href="../css/index/forgotten_style.css" rel="stylesheet">
+    <LINK href="../css/classes.css" rel="stylesheet">
+    <STYLE>
+        @media screen and (max-width: 500px){
+            main{
+                position: static !important;
+                height: 100% !important;
+            }
+        }
+    </STYLE>
 </HEAD>
 <BODY>
-    <MAIN>
+    <MAIN class='absolute50 backgroundblack8 border5px borderridge bordercolor100 centertext overflowauto'>
         <H1>Elfelejtett belépési adatok</H1>
-        <H2>A belépési adatok pótlásához adja meg felhasználónevét és/vagy e-mail címét!</H2>
-        <FORM method="POST" action="php/sendforgottendata.php">
-            <LABEL>Felhasználónév: <INPUT type="text" name="forgottenusername" id="forgottenusername" placeholder="Felhasználónév"></LABEL>
-            <LABEL>E-mail cím: <INPUT type="text" name="forgottenemail" id="forgottenemail" placeholder="E-mail cím"></LABEL>
-            <BUTTON>Adatok kérése</BUTTON>
+        <H2 class='fontsize20rem'>A belépési adatok pótlásához adja meg felhasználónevét és/vagy e-mail címét!</H2>
+        <FORM class='marginbottom30rem' method="POST" action="php/sendforgottendata.php">
+            <LABEL class='fontsize20rem block margin10rem'>Felhasználónév: <INPUT class='fontsize15rem' type="text" name="forgottenusername" id="forgottenusername" placeholder="Felhasználónév"></LABEL>
+            <LABEL class='fontsize20rem block margin10rem'>E-mail cím: <INPUT class='fontsize15rem' type="text" name="forgottenemail" id="forgottenemail" placeholder="E-mail cím"></LABEL>
+            <BUTTON class='fontsize15rem margintop15rem'>Adatok kérése</BUTTON>
         </FORM>
-        <A href="../index.php">Kezdőlap</A>
+        <A class='fontsize20rem' href="../index.php">Kezdőlap</A>
     </MAIN>
 </BODY>
 </HTML>
