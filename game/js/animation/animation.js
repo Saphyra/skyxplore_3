ed04fe693ae499f5dfcc82e3be6c87358a8b253e
Bernat Bonda <bondabear@gmail.com>
2018-03-06 21:37:54 +0100

    Moving map is quicker, and does not use window as storage

--- game/js/animation/animation.js
+++ game/js/animation/animation.js
@@ -1,6 +1,7 @@
 function Animation(){
     this.addWindowStateChangeListener = function addWindowStateChangeListener(){
         try{
+            document.addEventListener("keydown", e => { if(e.key == "F11") e.preventDefault(); });
             $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e){setButtonText();});
             $(window).on('resize', function(e){setButtonText();});
         }catch(err){
@@ -43,22 +44,24 @@ function Animation(){
         try{
             let lastX = 0;
             let lastY = 0;
+            this.clicked = false;
+            this.clickedButton;
             const map = document.getElementById("map");
                 //Egérkattintások mappelése
                 map.onmousedown = function(event){
                     lastX = event.clientX;
                     lastY = event.clientY;
-                    window.clicked = true;
-                    window.clickedButton = event.button;
+                    this.clicked = true;
+                    this.clickedButton = event.button;
                 };
-                map.onmouseup = function(){window.clicked = false;};
+                map.onmouseup = function(){this.clicked = false;};
                 
                 //Térkép mozgatása
                 map.onmousemove = function(event){
-                    if(window.clicked && window.clickedButton == 2){
+                    if(this.clicked && this.clickedButton == 2){
                         const diffX = lastX - event.clientX;
                         const diffY = lastY - event.clientY;
-                        document.getElementById("mapcontainer").scrollBy(diffX * 1.5, diffY * 2);
+                        document.getElementById("mapcontainer").scrollBy(diffX * 1.5, diffY * 2.5);
                         lastX = event.clientX;
                         lastY = event.clientY;
                     }
